e7323f1b33b15ed58f647ebc148005bfe384a6b8
filip-k06 <110892683+filip-k06@users.noreply.github.com>
2024-06-22 17:48:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -51,16 +51,6 @@ function loadPage(page) {
                     </div>
                 </div>
                 <div class="content inner">
-                    <h2>Meine Hobbys</h2>
-                    <div class="image-box">
-                        <img src="Zontes.jpg" alt="Mein Motorrad" class="image-text image-small">
-                        <div class="text-overlay">
-                            <h1>Freizeitaktivitäten</h1>
-                            <h3>In meiner Freizeit halte ich mich gerne körperlich fit, sei es im Fitnesszentrum oder auf meinem Motorrad (Zontes ZT 125). Früher war ich auch aktiv im Boxclub. Diese Aktivitäten helfen mir nicht nur, einen Ausgleich zum Schulalltag zu finden, sondern fördern auch meine Disziplin und Belastbarkeit – Qualitäten, die ich auch in mein Arbeitsumfeld einbringe.</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="content inner">
                     <h2>Schulische Ausbildung</h2>
                     <div class="zeitstrahl">
                         <div class="container-zeitstrahl left">
@@ -170,7 +160,7 @@ function loadPage(page) {
                         </div>
 
                         <div id="design" class="tabcontent">
-                            <h2>Projektmanagement und Entwicklungsumgebungen</h2>
+                            <h2>Projektmanagement und Systeme</h2>
                             <div class="skill-box">
                                 <span><h3>IPERKA</h3></span>
                                 <div class="skill-box">
@@ -258,6 +248,7 @@ function loadPage(page) {
                         </div>
                     </div>
                 </div>
+                <!--
                 <div class="content inner">
                     <h2>Portfolios</h2>
                     <div class="portfolio-slider">
@@ -270,24 +261,24 @@ function loadPage(page) {
                                 </div>
                             </div>
                             <div class="slide">
-                                <img src="page-0.png" alt="Portfolio 2">
+                                <img src="page-1.png" alt="Portfolio 2">
                                 <div class="slide-content">
                                     <h3>Portfolio 2</h3>
                                     <p>Beschreibung des Portfolios 2.</p>
                                 </div>
                             </div>
                             <div class="slide">
-                                <img src="page-0.png" alt="Portfolio 3">
+                                <img src="page-2.png" alt="Portfolio 3">
                                 <div class="slide-content">
                                     <h3>Portfolio 3</h3>
                                     <p>Beschreibung des Portfolios 3.</p>
                                 </div>
                             </div>
-                            <!-- Weitere Slides hier -->
                         </div>
                         <button class="prev-slide" onclick="changeSlide(-1)">&#10094;</button>
                         <button class="next-slide" onclick="changeSlide(1)">&#10095;</button>
                     </div>
+                    -->
                 </div>
                 <div class="content inner">
                     <h2>Portfolios mit Direktzugriff</h2>
@@ -301,6 +292,23 @@ function loadPage(page) {
                             <li><a href="https://portfolio.bbbaden.ch/user/f-kritzner-inf22/data-definition-language-ddl">Data Definition Language (DDL)</a></li>
                             <li><a href="https://portfolio.bbbaden.ch/user/f-kritzner-inf22/trycatch-modul319">TryCatch Modul 319</a></li>
                         </ul>
+                    </div>
+                </div>
+                <div class="content inner hobbys-pc">
+                    <h2>Meine Hobbys</h2>
+                    <div class="image-box">
+                        <img src="Zontes.jpg" alt="Mein Motorrad" class="image-text image-small">
+                        <div class="text-overlay">
+                            <h1>Freizeitaktivitäten</h1>
+                            <h3>In meiner Freizeit halte ich mich gerne körperlich fit, sei es im Fitnesszentrum oder auf meinem Motorrad (Zontes ZT 125). Früher war ich auch aktiv im Boxclub. Diese Aktivitäten helfen mir nicht nur, einen Ausgleich zum Schulalltag zu finden, sondern fördern auch meine Disziplin und Belastbarkeit – Qualitäten, die ich auch in mein Arbeitsumfeld einbringe.</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="content inner hobbys-mobile">
+                    <h2>Meine Hobbys</h2>
+                    <p>In meiner Freizeit halte ich mich gerne körperlich fit, sei es im Fitnesszentrum oder auf meinem Motorrad (Zontes ZT 125). Früher war ich auch aktiv im Boxclub. Diese Aktivitäten helfen mir nicht nur, einen Ausgleich zum Schulalltag zu finden, sondern fördern auch meine Disziplin und Belastbarkeit – Qualitäten, die ich auch in mein Arbeitsumfeld einbringe.</p>
+                    <div class="image-box centered">
+                        <img src="Zontes.jpg" alt="Mein Motorrad" class="zertifikat">
                     </div>
                 </div>`;
             break;
